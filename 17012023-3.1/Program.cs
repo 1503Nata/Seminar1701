d2cf6e553c3a6dae64b6978b4ae0a0bd0946d643
Natalya <pauls88@yandex.ru>
2023-01-22 23:16:06 +0500

    Addnewfile

---
+++ 17012023-3.1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите число от 1 до 7 соответсвующего дня недели:   ");
+int numday=Convert.ToInt32(Console.ReadLine());
+
+if(numday<=5) 
+{
+    Console.WriteLine("Это рабочий день");
+}
+if(numday==6 || numday==7) 
+{
+    Console.WriteLine("Это выходной день.");
+}
+if(numday>7)
+{
+   Console.WriteLine("Вы ввели неправильное число. В неделе всего семь дней");
+}
---
+++ 17012023-3/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+Console.Write("Введите число от 1 до 7 соответсвующего дня недели:   ");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+case(1):
+  Console.WriteLine("Понедельник - день рабочий");
+  break;
+case(2):
+  Console.WriteLine("Вторник - день рабочий");
+  break;  
+case(3):
+  Console.WriteLine("Среда - день рабочий.");
+  break; 
+case(4):
+  Console.WriteLine("Четверг -день рабочий.");
+  break;
+case(5):
+  Console.WriteLine("Пятница - день рабочий.");
+  break;
+case(6):
+  Console.WriteLine("Суббота - выходной день.");
+  break;
+case(7):
+  Console.WriteLine("Воскресенье - выходной день.");
+  break;
+default:
+  Console.WriteLine("Вы ввели неправильное число. В неделе всего семь дней.");
+  break;
+}
